c5b7ad88802de9c61459ef969b509f2359e0c77e
SenagaByakko <shogo.m.abiko.1213@gmail.com>
2023-11-03 09:43:24 +0000

    首Z軸のP制御を実装

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@
 #define NECK_RX_ACCELERATION 0.2
 #define NECK_RY_THRESHOLD 15
 #define NECK_RY_ACCELERATION 0.4
-#define NECK_RZ_THRESHOLD 30
-#define NECK_RZ_ACCELERATION 0.4
+#define NECK_RZ_THRESHOLD 25
+#define NECK_RZ_ACCELERATION 0.8
 #define CHIN_MAX 1900
 #define CHIN_MIN 1360
 #define CHIN_ACCELERATION 0.2
@@ -42,9 +42,9 @@ int neck_rz_sign_pool = 0x00;
 
 int extended_sign = 0x00;
 int chin_sign = 0x00;
-int neck_rx_sign = 0x00;
+int neck_rx_sign = 0x80;
 int neck_ry_sign = 0x00;
-int neck_rz_sign = 0x00;
+int neck_rz_sign = 0x80;
 int check_sum_sign = 0x00;
 int reply_sign = 0x0000;
 
@@ -55,10 +55,11 @@ int main()
 {
     initRobot();
     while (true) {
+        PC.printf("\033[Hneck-ry%5d\r\nneck-rz%5d\r\n", RE_neckry.Get_Count(), RE_neckrz.Get_Count());
         driveNeckRX();
         driveNeckRY();
         driveNeckRZ();
-        driveChin();
+        driveChin();        
     }
 }
 
@@ -100,16 +101,28 @@ void driveNeckRY(void) {
 }
 
 void driveNeckRZ(void) {
-    int target_count = (neck_rz_sign - 0x80) * 2;
-    double current_count = RE_neckrz.Get_Count();
+    int target_count = (int)((neck_rz_sign - 0x80) * 1.5);
+    double current_count = -RE_neckrz.Get_Count();
     double current_speed = M_neckrz.read();
     double target_speed;
     
-    if(target_count - NECK_RZ_THRESHOLD > current_count) target_speed = 1.0;
-    else if(target_count + NECK_RZ_THRESHOLD < current_count) target_speed = -1.0;
+    if(target_count - NECK_RZ_THRESHOLD > current_count) {
+        target_speed = (target_count - current_count) / 50;
+        if(target_speed > 0.85) {
+            target_speed = 0.85;
+        }
+        target_speed *= 0.8;
+    }
+    else if(target_count + NECK_RZ_THRESHOLD < current_count) {
+        target_speed = (target_count - current_count) / 50;
+        if(target_speed < -0.85) {
+            target_speed = -0.85;
+        }
+    }
     else target_speed = 0;
 
     current_speed += (target_speed - current_speed) * NECK_RZ_ACCELERATION;
+    PC.printf("target :%4d\r\nM_neckrz:%5.2lf\r\n", target_count, current_speed);
     M_neckrz.drive(current_speed); 
 }
 
